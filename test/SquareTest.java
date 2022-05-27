f3170417e39855c4c28d065f917b248249132259
mahsaShv <Msheikhi@ce.sharif.edu>
2022-05-27 22:51:36 +0430

    Change side and test added

--- test/SquareTest.java
+++ test/SquareTest.java
@@ -9,5 +9,16 @@ class SquareTest {
         double area = square.computeArea();
         assertEquals(36.0, area);
     }
+    @Test
+    public void ChangeSide() {
+        Square square = new Square(6);
+        assertEquals(36.0, square.computeArea());
 
+        square.setSide(4);
+        assertEquals(16.0, square.computeArea());
+
+        square.setSide(5);
+        double side = square.getSide();
+        assertEquals(side*side, square.computeArea());
+    }
 }
