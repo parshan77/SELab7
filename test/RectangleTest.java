3aef7dd29c05246f7c5aa6d49ff0e0fb6055e1c8
parshan77 <parshanteimouri@yahoo.com>
2022-05-27 21:52:54 +0430

    test file modified

--- test/RectangleTest.java
+++ test/RectangleTest.java
@@ -6,5 +6,11 @@ class RectangleTest {
 
     @Test
     void computeArea() {
+        Rectangle rectangle = new Rectangle(3, 7);
+        double area = rectangle.computeArea();
+        assertEquals(21.0, area);
+
+        Rectangle rectangle1 = new Rectangle(4, 7);
+        assertEquals(28.0, rectangle1.computeArea());
     }
 }
